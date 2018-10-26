d4b08505421a22f4f1dc06bd635b8f887abf6086
Eriasuitor <loryjiang@vanchu.net>
2018-10-26 19:27:31 +0800

    happy

---
+++ account_201810261924.sql
@@ -0,0 +1,19 @@
+INSERT INTO ic.account (userId,password,createDate,lastEditData,createUser,lastEditUser) VALUES 
+('123','123','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+,('234','234','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+,('3','lj60','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+,('345','lj60','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+,('5','lj60','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+,('567','lj60','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+,('789','lj60','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+,('9','lj60','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+,('999','lj60','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+,('Lory.Y.Jiang','lj60','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+;
+INSERT INTO ic.account (userId,password,createDate,lastEditData,createUser,lastEditUser) VALUES 
+('Lory.Y.Jiang2','lj60','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+,('Lory.Y.Jiang4','lj60','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+,('Lory.Y.Jiang6','lj60','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+,('Lory.Y.Jiang7','lj60','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+,('Lory.Y.Jiang8','lj60','2018-10-22 00:00:00.000','2018-10-22 00:00:00.000','Lory.Y.Jiang','Lory.Y.Jiang')
+;
---
+++ msg_record_201810261925.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO ic.msg_record (userId,`from`,`type`,content,createDate,lastEditData,createUser,lastEditUser) VALUES 
+('123','lory.y.jiang','text','dadsada','2018-10-26 19:12:45.000','2018-10-26 19:12:45.000','lory.y.jiang','lory.y.jiang')
+,('123','lory.y.jiang','text','asdsa ','2018-10-26 19:13:07.000','2018-10-26 19:13:07.000','lory.y.jiang','lory.y.jiang')
+,('3','3','text','啊发发','2018-10-26 19:21:23.000','2018-10-26 19:21:23.000','123','123')
+,('123','lory.y.jiang','text','你好','2018-10-26 19:22:50.000','2018-10-26 19:22:50.000','lory.y.jiang','lory.y.jiang')
+;
